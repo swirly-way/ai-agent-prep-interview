ffd7beec052cf49c212f0226826a65c76611e816
swirly-way <s4m14z1l@gmail.com>
2025-04-22 19:26:38 +0100

    Implement form for sign-in/up pages by using ShadCN form components and Zod's type validation schema

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return <div className="auth-layout">{children}</div>;
 };
 
 export default AuthLayout;
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import AuthForm from "@/components/AuthForm";
 
 const Page = () => {
-  return <div>Sign In</div>;
+  return <AuthForm type="sign-in" />;
 };
 
 export default Page;
