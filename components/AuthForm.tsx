72492158e0cb618f56fae4ff811a67d4ae3369dc
swirly-way <s4m14z1l@gmail.com>
2025-04-24 18:13:47 +0100

    Implement conditional rendering of elements on UI depending on sign-in/up page with a conditional link directing to one of each

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -7,22 +7,15 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { Form } from "@/components/ui/form";
+import Link from "next/link";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
 });
 
-const AuthForm = () => {
+const AuthForm = ({ type }: { type: FormType }) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -33,29 +26,43 @@ const AuthForm = () => {
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
   }
+
+  const isSignIn = type === "sign-in";
+
   return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
+    <div className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </div>
+        <h3> Practice job interviews with AI</h3>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6 mt-4 form"
+          >
+            {!isSignIn && <p>Name</p>}
+            <p>Email</p>
+            <p>Password</p>
+
+            <Button className="btn" type="submit">
+              {isSignIn ? "Sign in" : "Create an Account"}
+            </Button>
+          </form>
+        </Form>
+
+        <p className="text-center">
+          {isSignIn ? "No account yet?" : "Have an account already?"}
+          <Link
+            href={!isSignIn ? "/sign-in" : "/sign-up"}
+            className="font-bold text-user-primary ml-1"
+          >
+            {!isSignIn ? "Sign In" : "Sign Up"}
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
---
+++ components/FormField.tsx
@@ -0,0 +1,25 @@
+import {
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const FormField = () => (
+  <FormField
+    control={form.control}
+    name="username"
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel>Username</FormLabel>
+        <FormControl>
+          <Input placeholder="shadcn" {...field} />
+        </FormControl>
+        <FormDescription>This is your public display name.</FormDescription>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
