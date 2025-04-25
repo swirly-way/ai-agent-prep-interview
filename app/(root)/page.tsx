105ec2521a320417cbc7fae6718788732c0e5fbb
swirly-way <s4m14z1l@gmail.com>
2025-04-25 16:39:16 +0100

    Create card with call to action, clickable button and image rendering on non-mobile devices

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 const Page = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions & get instant feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+    </>
+  );
 };
 
 export default Page;
